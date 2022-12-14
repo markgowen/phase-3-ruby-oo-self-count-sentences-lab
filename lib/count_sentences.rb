7adfb65b40c997b07b4087cf360d664de115e618
Mark <mark.gowen78@gmail.com>
2022-12-14 14:44:13 -0800

    Lab solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
+# require "pry"
 
 class String
-
+  #define a method that returns the number of sentences in a string
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #use split and filter to count the number of sentences
+    self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
   end
 end
